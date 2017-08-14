c522bdb0f8adbdd19bd1f300b5081d59e679d2be
Kevin Harrison <kharrison7@elon.edu>
2017-08-14 14:31:50 -0400

    This makes the first desired line appear.

--- app/index.js
+++ app/index.js
@@ -1,29 +1,44 @@
 // app/index.js
-// const calc = require('./calc')
+
+// This calls in the chalk module.
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
+// This calls in the moment module.
+let moment = require('moment');
+moment().format();
+console.log(moment());
+
+let now = moment().format('LLLL');
+
+console.log(`It is ${chalk.blue(now)}.`);
+// console.log(`It is ${chalk.blue(moment(now, "MM-DD-YYYY").toDate())}`);
+
+
+
+
+
+
+
+// From date to moment
+// var wrapped = moment(new Date());
+// console.log(wrapped);
+
+// From moment to date
+// var date = wrapped.toDate();
+// console.log(date);
+
+
+
 
 // This calls the readline module.
 const readline = require('readline');
-
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-rl.question('What is the input text?', (answer) => {
-
-// This sends the data to calc and reurns a result.
-// const result = calc.sum(numbersToAdd1, numbersToAdd2)
-let double;
-
-function sum (num) {
-    double = num*2;
-    return double;
-  }
-
-sum(answer);
-
+ rl.question('Would you like to know the date information?', (answer) => {
   // console.log(`${answer}has ${answer.length} characeters.`);
-  console.log(`${answer} doubled is: ${double}`);
+  console.log(`You said: ${answer}`);
   rl.close();
 });
